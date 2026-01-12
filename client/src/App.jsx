b95eb1aa9885714e39a38c2ff2ce93815f42f7ed
chirag-says <xkyzerop@gmail.com>
2026-01-12 22:46:11 +0530

    fix: add AuthProvider wrapper and remove broken style.css reference - Fix 'useAuth must be used within AuthProvider' error - Remove /src/style.css link that caused 404 in production - Update page title to DealDirect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,7 @@ import TermsAndConditions from "./Pages/Terms&Conditions/Terms&Conditions.jsx";
 import WhyUs from "./Pages/WhyUs/WhyUs.jsx";
 import Pricing from "./Pages/Pricing/Pricing.jsx";
 import ScrollToTop from "./Components/ScrollToTop/ScrollToTop.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import { ChatProvider } from "./context/ChatContext.jsx";
 import ChatWidget from "./Components/Chat/ChatWidget.jsx";
 import ChatButton from "./Components/Chat/ChatButton.jsx";
@@ -29,33 +30,35 @@ function App() {
   return (
     <div className="w-full min-h-screen overflow-x-hidden bg-white pt-16 lg:pt-20">
       <Router>
-        <ChatProvider>
-          <ScrollToTop />
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/properties" element={<PropertyPage />} />
-            <Route path="/properties/:id" element={<PropertyDetails />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/agreements" element={<AgreementGenerator />} />
-            <Route path="/add-property" element={<AddProperty />} />
-            <Route path="/edit-property/:id" element={<EditProperty />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/my-properties" element={<MyProperties />} />
-            <Route path="/saved-properties" element={<SavedProperties />} />
-            <Route path="/notifications" element={<Notifications />} />
-            <Route path="/privacy" element={<PrivacyPolicy />} />
-            <Route path="/terms" element={<TermsAndConditions />} />
-            <Route path="/why-us" element={<WhyUs />} />
-            <Route path="/pricing" element={<Pricing />} />
-          </Routes>
-          <Footer />
-          <ChatButton />
-          <ChatWidget />
-        </ChatProvider>
+        <AuthProvider>
+          <ChatProvider>
+            <ScrollToTop />
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/properties" element={<PropertyPage />} />
+              <Route path="/properties/:id" element={<PropertyDetails />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/agreements" element={<AgreementGenerator />} />
+              <Route path="/add-property" element={<AddProperty />} />
+              <Route path="/edit-property/:id" element={<EditProperty />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/my-properties" element={<MyProperties />} />
+              <Route path="/saved-properties" element={<SavedProperties />} />
+              <Route path="/notifications" element={<Notifications />} />
+              <Route path="/privacy" element={<PrivacyPolicy />} />
+              <Route path="/terms" element={<TermsAndConditions />} />
+              <Route path="/why-us" element={<WhyUs />} />
+              <Route path="/pricing" element={<Pricing />} />
+            </Routes>
+            <Footer />
+            <ChatButton />
+            <ChatWidget />
+          </ChatProvider>
+        </AuthProvider>
       </Router>
     </div>
   );
